0a1785b7df890f7be2712c9ae60f58fc4cee938a
Barbosa <joaovictorbarbosa2@gmail.com>
2025-03-25 10:06:03 -0300

    Adição do Read, Update e Delete. E tambem, incluindo os valores com input, ao inves de usar valores pré settados.

--- CRUD/crud.py
+++ CRUD/crud.py
@@ -12,15 +12,32 @@ conexao = mysql.connector.connect(
 cursor = conexao.cursor()
 
 #CREATE
-nome_produto = "chocolate"
-valor = 8
+nome_produto = str(input('DIGITE O NOME DO PRODUTO: '))
+valor = float(input('DIGITE O VALOR DO PRODUTO (EM REAIS): '))
 comando = f'INSERT INTO vendas (nome_produto, valor) VALUES ("{nome_produto}", {valor})'
 cursor.execute(comando)
 #quando editamos o banco de dados, com update ou delete, usamos:
 conexao.commit()
 # para ler o banco de dados
-#resultado = cursor.fetchall()
 
+#READ
+comando = f'SELECT * from vendas;'
+cursor.execute(comando)
+resultado = cursor.fetchall()
+print(resultado)
+
+#UPDATE
+idVendas = int(input('DIGITE O ID DO PRODUTO: '))
+valor_novo = float(input('DIGITE QUAL O NOVO VALOR: '))
+comando = f'UPDATE db_crud.vendas SET valor = {valor_novo} WHERE idVendas = {idVendas}'
+cursor.execute(comando)
+conexao.commit()
+
+#DELETE
+idVendas = int(input('DIGITE O ID DO PRODUTO QUE DESEJA EXCLUIR: '))
+comando = f'DELETE FROM db_crud.vendas WHERE idVendas = "{idVendas}"'
+cursor.execute(comando)
+conexao.commit()
 
 cursor.close()
 conexao.close()
